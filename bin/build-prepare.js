f33fd1b72f18aea8948dc17b4d0b6de653c3308e
Sanemat <o.gata.ken@gmail.com>
2017-12-17 19:22:37 +0900

    chore(install): npm install --production

--- bin/build-prepare.js
+++ bin/build-prepare.js
@@ -2,6 +2,7 @@ const rimraf = require('rimraf');
 const pify = require('pify');
 const copy = require('copy-concurrently');
 const path = require('path');
+const spawn = require('cross-spawn');
 
 const targets = ['dist-firefox', 'dist-chrome'];
 const files = ['package.json', 'package-lock.json', 'license', 'changelog.md'];
@@ -15,5 +16,15 @@ Promise.all(targets.map((target) => {
     return Promise.all(files.map((file) => {
       return copy(file, path.join(target, file));
     }));
+  }).then(() => {
+    return spawn.sync(
+      'npm',
+      ['install', '--production'],
+      {
+        cwd: target,
+        stdio: 'inherit',
+        env: process.env,
+      }
+    );
   });
 }));
