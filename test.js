43f6b252f5a54183a653b34cb08d6314a29e07da
Sanemat <o.gata.ken@gmail.com>
2017-12-17 17:16:48 +0900

    chore(test): fix

--- test.js
+++ test.js
@@ -5,6 +5,3 @@ var extensionPageForHatenaBookmark = require('./');
 it('should ', function () {
   assert.strictEqual(extensionPageForHatenaBookmark('unicorns'), 'unicorns & rainbows');
 });
-it('should not ', function () {
-  assert.strictEqual(extensionPageForHatenaBookmark('unicorns'), 'unicorns & wrong');
-});
