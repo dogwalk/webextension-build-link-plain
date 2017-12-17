9e6a23265fda8e531321b24965cdfd18c40bf150
Sanemat <o.gata.ken@gmail.com>
2017-12-17 19:27:10 +0900

    style(build): wrap promise resolve then

--- bin/build-prepare.js
+++ bin/build-prepare.js
@@ -7,24 +7,26 @@ const spawn = require('cross-spawn');
 const targets = ['dist-firefox', 'dist-chrome'];
 const files = ['package.json', 'package-lock.json', 'license', 'changelog.md', 'readme.md'];
 
-Promise.all(targets.map((target) => {
-  Promise.resolve().then(() => {
-    return pify(rimraf)(target);
-  }).then(() => {
-    return copy('src', target);
-  }).then(() => {
-    return Promise.all(files.map((file) => {
-      return copy(file, path.join(target, file));
-    }));
-  }).then(() => {
-    return spawn.sync(
-      'npm',
-      ['install', '--production'],
-      {
-        cwd: target,
-        stdio: 'inherit',
-        env: process.env,
-      }
-    );
-  });
-}));
+Promise.resolve().then(() => {
+  return Promise.all(targets.map((target) => {
+    Promise.resolve().then(() => {
+      return pify(rimraf)(target);
+    }).then(() => {
+      return copy('src', target);
+    }).then(() => {
+      return Promise.all(files.map((file) => {
+        return copy(file, path.join(target, file));
+      }));
+    }).then(() => {
+      return spawn.sync(
+        'npm',
+        ['install', '--production'],
+        {
+          cwd: target,
+          stdio: 'inherit',
+          env: process.env,
+        }
+      );
+    });
+  }));
+});
