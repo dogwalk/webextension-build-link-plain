a282663429d43117da223eb26f7b47a9f09ec5d5
Sanemat <o.gata.ken@gmail.com>
2017-12-17 18:19:07 +0900

    chore(build): copy

--- bin/bulid-prepare.js
+++ bin/bulid-prepare.js
@@ -1,11 +1,13 @@
 const rimraf = require('rimraf');
 const pify = require('pify');
-// const copy = require('copy-concurrently');
+const copy = require('copy-concurrently');
 
 const targets = ['dist-firefox', 'dist-chrome'];
 
 for (const target of targets) {
   Promise.resolve().then(() => {
     return pify(rimraf)(target);
+  }).then(() => {
+    return copy('src', `dist-${target}`);
   });
 }
