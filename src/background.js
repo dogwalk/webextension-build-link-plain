a830c6ad559e0c485355543d3e1ffeb8dcbca3f7
Sanemat <o.gata.ken@gmail.com>
2017-12-20 09:01:03 +0900

    chore(content): run only after clicking

--- src/background.js
+++ src/background.js
@@ -1,3 +1,23 @@
-browser.browserAction.onClicked.addListener(() => {
-  console.log('build!'); // eslint-disable-line no-console
-});
+/**
+ * There was an error executing the script.
+ * Display the error message.
+ */
+function reportExecuteScriptError(error) {
+  console.error(`Failed to execute content script: ${error.message}`); // eslint-disable-line no-console
+}
+
+function gatherPageInfo() {
+  browser.tabs.executeScript({file: '/content_script.js'})
+    .catch(reportExecuteScriptError);
+}
+
+browser.browserAction.onClicked.addListener(gatherPageInfo);
+
+function notify(message) {
+  console.log(message.join(' ')); // eslint-disable-line no-console
+}
+
+/*
+Assign `notify()` as a listener to messages from the content script.
+*/
+browser.runtime.onMessage.addListener(notify);
