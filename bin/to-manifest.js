655095bb9e266f2f4fb73e95addd45631519e80c
Sanemat <o.gata.ken@gmail.com>
2017-12-31 12:01:11 +0900

    chore(manifest): remove reserved name

--- bin/to-manifest.js
+++ bin/to-manifest.js
@@ -4,19 +4,19 @@ const path = require('path');
 
 const targets = ['dist-firefox', 'dist-chrome'];
 
-let package;
+let packageMeta;
 let manifest;
 Promise.resolve().then(() => {
-  return pify(fs.readFile)('package.json', 'utf-8');
+  return pify(fs.readFile)('packageMeta.json', 'utf-8');
 }).then(data => {
-  package = JSON.parse(data);
+  packageMeta = JSON.parse(data);
 }).then(() => {
   return pify(fs.readFile)(path.join('webextension', 'manifest.json'), 'utf-8');
 }).then(data => {
   manifest = JSON.parse(data);
 }).then(() => {
   return Promise.all(targets.map(target => {
-    manifest.version = package.version;
+    manifest.version = packageMeta.version;
     return pify(fs.writeFile)(path.join(target, 'manifest.json') ,JSON.stringify(manifest, null, 2));
   }));
 }).catch(error => {
